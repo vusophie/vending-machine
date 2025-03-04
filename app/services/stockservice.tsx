0a56f7384c170113b63732b8a1423c60df3fcae8
Sophie Vu <sottvu@gmail.com>
2025-03-03 22:48:56 -0500

    correct stock of cola

--- app/services/stockservice.tsx
+++ app/services/stockservice.tsx
@@ -8,7 +8,7 @@ export type Item = {
 
 class StockService {
   private stock: Item[] = [
-    { amount: 1, drink: "Cola", price: 25, img: "/cola.png" },
+    { amount: 10, drink: "Cola", price: 25, img: "/cola.png" },
     { amount: 8, drink: "Diet Cola", price: 35, img: "/diet.png" },
     { amount: 0, drink: "Lime Soda", price: 25, img: "/sprite.png" },
     { amount: 2, drink: "Water", price: 45, img: "/water.png" },
@@ -19,13 +19,10 @@ class StockService {
   }
 
   updateStock(drink: string, quantity: number) {
-    console.log(`updateStock called for: ${drink}, Quantity: ${quantity}`);
   
     this.stock = this.stock.map((item) => {
       if (item.drink === drink) {
-        console.log(`Before update: ${item.amount}`);
         const newAmount = item.amount - quantity;
-        console.log(`After update: ${newAmount}`);
         return { ...item, amount: newAmount };
       }
       return item;
